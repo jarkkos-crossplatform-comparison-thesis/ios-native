554412838681d19440257a67420609eac0f19ba3
Jarkko Saarinen <jsaarinen92@gmail.com>
2018-08-18 20:58:47 +0300

    Empty iOS Swift project

---
+++ Thesis comparison iOSTests/Thesis_comparison_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Thesis_comparison_iOSTests.swift
+//  Thesis comparison iOSTests
+//
+//  Created by Jarkko Saarinen on 18/08/2018.
+//  Copyright © 2018 Jarkko Saarinen. All rights reserved.
+//
+
+import XCTest
+@testable import Thesis_comparison_iOS
+
+class Thesis_comparison_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
