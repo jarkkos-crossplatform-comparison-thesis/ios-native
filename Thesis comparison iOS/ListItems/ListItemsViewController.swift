2bf5e692307ff8e36769e5c56cbfd083cb124a97
Jarkko Saarinen <jarkko.saarinen@uta.fi>
2018-12-04 20:00:22 +0200

    Change package structure, make back button work on touch down

---
+++ Thesis comparison iOS/ListItems/ListItemsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  ListItemsScreen.swift
+//  Thesis comparison iOS
+//
+//  Created by Jarkko Saarinen on 04/12/2018.
+//  Copyright © 2018 Jarkko Saarinen. All rights reserved.
+//
+
+import Foundation
--- Thesis comparison iOS/NoAnimationPopUIViewController.swift
+++ Thesis comparison iOS/NoAnimationPopUIViewController.swift
@@ -12,10 +12,16 @@ import UIKit
 class NoAnimationPopUIViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.leftBarButtonItem = UIBarButtonItem(title: "Back", style: .plain, target: self, action: #selector(backTapped))
+        
+        let backButton = UIButton(type: .system)
+        backButton.setTitle("Back", for: .normal)
+        backButton.frame = CGRect(x: 0.0, y: 0.0, width: 40.0, height: 30.0)
+        backButton.addTarget(self, action: #selector(backTapped), for: .touchDown)
+        
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(customView: backButton)
     }
     
-    @objc func backTapped(sender: UIBarButtonItem) {
+    @objc func backTapped(sender: UIButton) {
         navigationController?.popViewController(animated: false)
     }
 }
