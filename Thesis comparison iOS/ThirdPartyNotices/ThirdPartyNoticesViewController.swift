db9756e7573afb738cf8bcc8745125ad8aca960e
Jarkko Saarinen <jarkko.saarinen@uta.fi>
2018-12-19 17:51:29 +0200

    Remove back animation from third party notices screen. Add app icons

---
+++ Thesis comparison iOS/ThirdPartyNotices/ThirdPartyNoticesViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ThirdPartyNoticesViewController.swift
+//  Thesis comparison iOS
+//
+//  Created by Jarkko Saarinen on 09/12/2018.
+//  Copyright © 2018 Jarkko Saarinen. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ThirdPartyNoticesViewController: NoAnimationPopUIViewController {
+    
+}
